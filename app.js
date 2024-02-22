255e5d5ded42ca54693a9abe2ce3870f2976eb8e
adilrazakhan <159728598+adilrazakhan@users.noreply.github.com>
2024-02-23 02:07:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict";
+let message = "hello adil";
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+let numberone = 3;
+let numbertwo = 4;
+let sum = numberone + numbertwo;
+console.log(sum);
+let greetings = "hello adil";
+console.log(greetings);
+let calculateone = 2;
+let calculatetwo = 5;
+let add = calculateone + calculatetwo;
+console.log(add);
---
+++ app.ts
@@ -0,0 +1,23 @@
+let message = "hello adil";
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+console.log(message);
+
+let numberone = 3
+let numbertwo = 4
+let sum = numberone + numbertwo
+console.log(sum);
+let greetings : string = "hello adil";
+console.log(greetings);
+let calculateone: number = 2
+let calculatetwo: number = 5
+let add = calculateone+calculatetwo
+console.log(add);
+
+
+
+
+
